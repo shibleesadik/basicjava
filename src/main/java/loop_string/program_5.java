7d0496305b1fe80f1b8b09c2bcc8609d28b242a1
shibleesadik <shibleesadik@gmail.com>
2021-08-28 02:16:53 +0600

    Assignment_3

--- src/main/java/loop_string/program_5.java
+++ src/main/java/loop_string/program_5.java
@@ -1,4 +1,5 @@
 package loop_string;
+
 //  <html>
 //<body>
 //<div>
@@ -14,19 +15,21 @@ package loop_string;
 //Extract only transaction ID from the HTML
 public class program_5 {
     public static void main(String[] args) {
-    String content= " <html>\n" +
-            "<body>\n" +
-            "<div>\n" +
-            "<p>Transaction Successful</p>\n" +
-            "<p>Trnx ID: TXN5164870</p>\n" +
-            "</div>\n" +
-            "\n" +
-            "\n" +
-            "</body>\n" +
-            "</html>\n";
-        content = content.replaceAll("[^\\d]", " ");
-        content = content.trim();
-        System.out.println(content);
+        String content = " <html>\n" +
+                "<body>\n" +
+                "<div>\n" +
+                "<p>Transaction Successful</p>\n" +
+                "<p>Trnx ID: TXN5164870</p>\n" +
+                "</div>\n" +
+                "\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>\n";
+        int pos = content.indexOf("TXN");
+        System.out.println(content.substring(pos, 72));
+//        content = content.replaceAll("[^\\d]", " ");
+//        content = content.trim();
+//        System.out.println(content);
 
     }
 }
